7e88708301a68eadb84af7df2440afd19b47d434
jaiku <jaikumarb031@gmail.com>
2024-12-17 22:52:23 +0530

    First Commit

--- C2TC_VemanaIT/src/com/tnsif/day1/DataTypesDemo.java
+++ C2TC_VemanaIT/src/com/tnsif/day1/DataTypesDemo.java
@@ -7,6 +7,7 @@ public class DataTypesDemo {
 		float value2 = 10/3; //Float division
 		double value3 = 10d/6d;//Double division
 		
+		System.out.println("Printing the values");
 		System.out.println("Value 1:"+value1);
 		System.out.println("Value 2:"+value2);
 		System.out.println("Value 3:"+value3);
